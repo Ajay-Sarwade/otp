e6f17774ef8d8ead9bc0e7777a5b754013391bae
Ajay Sarwade <ajaysarwade@gmail.com>
2024-08-17 22:29:12 +0530

    otp initial commit

---
+++ src/components/Otp.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function Otp({ size = 4 }) {
+  const [otpInputs, setOtpInputs] = useState(new Array(size).fill(""));
+
+  //let ref be an array
+  const otpRefs = useRef([]);
+
+  useEffect(() => {
+    otpRefs.current[0].focus();
+  }, []);
+
+  function handleChange(e, index) {
+    let value = e.target.value;
+    // only 1,2,3 and empty strings " " are numbers
+    if (isNaN(value)) return;
+
+    //get the latest value entered
+    value = value[value.length - 1];
+
+    // always copy array/object by creating new one by spreading
+    let newOtpInputs = [...otpInputs];
+
+    newOtpInputs[index] = value;
+    setOtpInputs(newOtpInputs);
+
+    if (index < size - 1 && value) {
+      otpRefs.current[index + 1].focus();
+    }
+  }
+
+  function handleBackChange(e, index) {
+    if (e.key == "Backspace") {
+      if (otpRefs.current[index].value) {
+        otpRefs.current[index].focus();
+      } else if (index > 0) {
+        otpRefs.current[index - 1].focus();
+      }
+    }
+  }
+
+  return (
+    <div>
+      {otpInputs.map((value, index) => {
+        return (
+          <input
+            //important code
+            ref={(input) => (otpRefs.current[index] = input)}
+            type="text"
+            key={index}
+            value={value}
+            className="otpInput"
+            onChange={(e) => handleChange(e, index)}
+            onKeyDown={(e) => handleBackChange(e, index)}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Otp;
